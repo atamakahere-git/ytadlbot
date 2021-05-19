8f30f2631bf9a42044ea95262604f39f1d075930
M. Tanveer Raza <tanveerraza789@gmail.com>
2021-05-19 11:18:53 +0530

    Add postgre sql

--- databasehandler.py
+++ databasehandler.py
@@ -2,7 +2,7 @@ import psycopg2
 import urllib.parse as urlparse
 
 
-def start_dbhandler(audio_db: str) -> bool:
+def start_dbhandler(audio_db: str):
     result = urlparse.urlparse(audio_db)
     username = result.username
     password = result.password
@@ -17,22 +17,26 @@ def start_dbhandler(audio_db: str) -> bool:
         port=port
     )
     if conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS Audios("
-                     "yt_link TEXT PRIMARY KEY,"
-                     "msg_id INTEGER)")
-
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS Audios("
+                    "yt_link TEXT PRIMARY KEY,"
+                    "msg_id INTEGER)")
+        conn.commit()
         return conn
     return False
 
 
 def check_in_db(url, conn) -> int or None:
-    cursor = conn.execute("SELECT msg_id from Audios where yt_link = ?", (url,))
-    row = cursor.fetchall()
+    cur = conn.cursor()
+    cur.execute("SELECT msg_id from Audios where yt_link = ?", (url,))
+    row = cur.fetchall()
     if len(row) > 0:
         return int(row[0][0])
     return None
 
 
 def add_to_db(url: str, msg_id: int, conn):
-    conn.execute("INSERT INTO Audios(yt_link,msg_id)"
-                 "VALUES (?,?)", (url, msg_id))
+    cur = conn.cursor()
+    cur.execute("INSERT INTO Audios(yt_link,msg_id)"
+                "VALUES (?,?)", (url, msg_id))
+    conn.commit()
--- logger.py
+++ logger.py
@@ -10,7 +10,7 @@ SET {} = {}
 WHERE chat_id = ?;'''
 
 
-def start_logger(user_db: str) -> bool:
+def start_logger(user_db: str):
     global STATUS
     result = urlparse.urlparse(user_db)
     username = result.username
@@ -27,15 +27,16 @@ def start_logger(user_db: str) -> bool:
     )
     if conn:
         STATUS = True
-        conn.execute("CREATE TABLE IF NOT EXISTS Users("
-                     "chat_id INTEGER PRIMARY KEY,"
-                     "first_name TEXT,"
-                     "last_name TEXT,"
-                     "username TEXT,"
-                     "s_cmd INTEGER,"
-                     "h_cmd INTEGER,"
-                     "d_cmd INTEGER)")
-
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS Users("
+                    "chat_id INTEGER PRIMARY KEY,"
+                    "first_name TEXT,"
+                    "last_name TEXT,"
+                    "username TEXT,"
+                    "s_cmd INTEGER,"
+                    "h_cmd INTEGER,"
+                    "d_cmd INTEGER)")
+        conn.commit()
         return conn
     return False
 
@@ -50,17 +51,18 @@ def log(update: Update, conn):
     last_name = update.effective_chat.last_name
     username = update.effective_chat.username
     cmd = update.message.text.split(' ')[0][1:]
+    cur = conn.cursor()
     try:
-        conn.execute(SQL_BASE_CMD, (chat_id, f"'{first_name}'", f"'{last_name}'", f"'{username}'", 0, 0, 0))
+        cur.execute(SQL_BASE_CMD, (chat_id, f"'{first_name}'", f"'{last_name}'", f"'{username}'", 0, 0, 0))
     except Exception as e:
         print(e)
         try:
             if cmd == 'start':
-                conn.execute(SQL_UPDATE_CMD.format('s_cmd', 's_cmd+1'), (chat_id,))
+                cur.execute(SQL_UPDATE_CMD.format('s_cmd', 's_cmd+1'), (chat_id,))
             elif cmd == 'help':
-                conn.execute(SQL_UPDATE_CMD.format('h_cmd', 'h_cmd+1'), (chat_id,))
+                cur.execute(SQL_UPDATE_CMD.format('h_cmd', 'h_cmd+1'), (chat_id,))
             else:
-                conn.execute(SQL_UPDATE_CMD.format('d_cmd', 'h_cmd+1'), (chat_id,))
+                cur.execute(SQL_UPDATE_CMD.format('d_cmd', 'h_cmd+1'), (chat_id,))
         except:
             print(f"Unable to log : {chat_id}")
 
