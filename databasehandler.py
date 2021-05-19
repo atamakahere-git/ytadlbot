46e6e5b375f39a5ec8f2ce69b821633cbd25ed00
M. Tanveer Raza <tanveerraza789@gmail.com>
2021-05-19 11:33:21 +0530

    Add postgre sql

--- databasehandler.py
+++ databasehandler.py
@@ -1,42 +1,68 @@
 import psycopg2
 import urllib.parse as urlparse
 
+STATUS = False
+
 
 def start_dbhandler(audio_db: str):
-    result = urlparse.urlparse(audio_db)
-    username = result.username
-    password = result.password
-    database = result.path[1:]
-    hostname = result.hostname
-    port = result.port
-    conn = psycopg2.connect(
-        database=database,
-        user=username,
-        password=password,
-        host=hostname,
-        port=port
-    )
+    global STATUS
+    try:
+        result = urlparse.urlparse(audio_db)
+        username = result.username
+        password = result.password
+        database = result.path[1:]
+        hostname = result.hostname
+        port = result.port
+    except Exception as e:
+        print(e)
+        print("DB url is not valid")
+        STATUS = False
+        return None
+    try:
+        conn = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+        STATUS = True
+    except Exception as e:
+        print(e)
+        print("Connection establishment failed")
+        STATUS = False
+        return None
     if conn:
         cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS Audios("
-                    "yt_link TEXT PRIMARY KEY,"
-                    "msg_id INTEGER)")
+        try:
+            cur.execute("CREATE TABLE IF NOT EXISTS Audios("
+                        "yt_link TEXT PRIMARY KEY,"
+                        "msg_id INTEGER)")
+        except Exception as e:
+            print(e)
+            print("Table creation failed")
+            STATUS = False
+            return None
         conn.commit()
         return conn
     return False
 
 
 def check_in_db(url, conn) -> int or None:
-    cur = conn.cursor()
-    cur.execute("SELECT msg_id from Audios where yt_link = ?", (url,))
-    row = cur.fetchall()
-    if len(row) > 0:
-        return int(row[0][0])
+    if STATUS:
+        cur = conn.cursor()
+        cur.execute("SELECT msg_id from Audios where yt_link = {}".format(url))
+        row = cur.fetchall()
+        if len(row) > 0:
+            return int(row[0][0])
     return None
 
 
 def add_to_db(url: str, msg_id: int, conn):
-    cur = conn.cursor()
-    cur.execute("INSERT INTO Audios(yt_link,msg_id)"
-                "VALUES (?,?)", (url, msg_id))
-    conn.commit()
+    if STATUS:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Audios(yt_link,msg_id)"
+                    "VALUES ({},{})".format(url,msg_id))
+        conn.commit()
+    else:
+        print("DB is not active cannot add the entry!")
--- logger.py
+++ logger.py
@@ -4,10 +4,10 @@ from telegram import Update
 
 STATUS = False
 SQL_BASE_CMD = ''' INSERT INTO Users(chat_id,first_name,last_name,username,s_cmd,h_cmd,d_cmd)
-              VALUES(?,?,?,?,?,?,?) '''
+              VALUES({},{},{},{},{},{},{}) '''
 SQL_UPDATE_CMD = '''UPDATE Users
 SET {} = {}
-WHERE chat_id = ?;'''
+WHERE chat_id = {};'''
 
 
 def start_logger(user_db: str):
@@ -18,24 +18,36 @@ def start_logger(user_db: str):
     database = result.path[1:]
     hostname = result.hostname
     port = result.port
-    conn = psycopg2.connect(
-        database=database,
-        user=username,
-        password=password,
-        host=hostname,
-        port=port
-    )
+    try:
+        conn = psycopg2.connect(
+            database=database,
+            user=username,
+            password=password,
+            host=hostname,
+            port=port
+        )
+    except Exception as e:
+        print(e)
+        print("Connection establishment failed")
+        STATUS = False
+        return None
     if conn:
         STATUS = True
         cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS Users("
-                    "chat_id INTEGER PRIMARY KEY,"
-                    "first_name TEXT,"
-                    "last_name TEXT,"
-                    "username TEXT,"
-                    "s_cmd INTEGER,"
-                    "h_cmd INTEGER,"
-                    "d_cmd INTEGER)")
+        try:
+            cur.execute("CREATE TABLE IF NOT EXISTS Users("
+                        "chat_id INTEGER PRIMARY KEY,"
+                        "first_name TEXT,"
+                        "last_name TEXT,"
+                        "username TEXT,"
+                        "s_cmd INTEGER,"
+                        "h_cmd INTEGER,"
+                        "d_cmd INTEGER)")
+        except Exception as e:
+            print(e)
+            print("Table creation failed")
+            STATUS = False
+            return None
         conn.commit()
         return conn
     return False
@@ -44,6 +56,7 @@ def start_logger(user_db: str):
 def log(update: Update, conn):
     global STATUS
     if not STATUS:
+        print("DB is not connected, cannot log!")
         return
     global SQL_BASE_CMD, SQL_UPDATE_CMD
     chat_id = update.effective_chat.id
@@ -53,17 +66,18 @@ def log(update: Update, conn):
     cmd = update.message.text.split(' ')[0][1:]
     cur = conn.cursor()
     try:
-        cur.execute(SQL_BASE_CMD, (chat_id, f"'{first_name}'", f"'{last_name}'", f"'{username}'", 0, 0, 0))
+        cur.execute(SQL_BASE_CMD.format(chat_id, first_name, last_name, username, 0, 0, 0))
     except Exception as e:
         print(e)
         try:
             if cmd == 'start':
-                cur.execute(SQL_UPDATE_CMD.format('s_cmd', 's_cmd+1'), (chat_id,))
+                cur.execute(SQL_UPDATE_CMD.format('s_cmd', 's_cmd+1', chat_id))
             elif cmd == 'help':
-                cur.execute(SQL_UPDATE_CMD.format('h_cmd', 'h_cmd+1'), (chat_id,))
+                cur.execute(SQL_UPDATE_CMD.format('h_cmd', 'h_cmd+1', chat_id))
             else:
-                cur.execute(SQL_UPDATE_CMD.format('d_cmd', 'h_cmd+1'), (chat_id,))
-        except:
+                cur.execute(SQL_UPDATE_CMD.format('d_cmd', 'h_cmd+1', chat_id))
+        except Exception as e:
+            print(e)
             print(f"Unable to log : {chat_id}")
 
     conn.commit()
